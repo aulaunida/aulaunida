eab9a1c404b0d3aa6a639970089637e0f1ce1492
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2024-10-16 22:48:08 -0300

    53 Formulario de registro de informes finalizado OK

--- admin/informes/index.php
+++ admin/informes/index.php
@@ -67,6 +67,7 @@ include('../../app/controllers/estudiantes/listado_de_estudiantes.php');
                                         $id_grado = $asignacione['id_grado'];
                                         if ($email_sesion == $asignacione['email']) {
                                             $id_asignacion = $asignacione['id_asignacion'];
+                                            $docente_id = $asignacione['docente_id'];
                                             $contador = $contador + 1; ?>
                                             <tr>
                                                 <td>
@@ -89,7 +90,9 @@ include('../../app/controllers/estudiantes/listado_de_estudiantes.php');
                                                     <a class="btn btn-info btn-sm" data-toggle="modal"
                                                         data-target="#exampleModal<?= $id_asignacion; ?>"><i
                                                             class="bi bi-journals"></i> Redactar informe</a>
-                                                    <!-- Modal -->
+
+
+                                                    <!-- INICIO DEL MODAL REGISTRAR INFORME  -->
                                                     <div class="modal fade" id="exampleModal<?= $id_asignacion; ?>"
                                                         tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                         <div class="modal-dialog modal-lg">
@@ -106,52 +109,120 @@ include('../../app/controllers/estudiantes/listado_de_estudiantes.php');
                                                                         <span aria-hidden="true">&times;</span>
                                                                     </button>
                                                                 </div>
-                                                                <div class="modal-body">
-                                                                    <div class="row">
-                                                                        <div class="col-md-12">
-                                                                            <div class="form-group" style="text-align: left">
-                                                                                <label for="" class="text-align-left">Fecha de
-                                                                                    Informe</label>
-                                                                                <input type="date" class="form-control" name=""
-                                                                                    id="">
+                                                                <form action="<?=APP_URL;?>/app/controllers/informes/create.php" method="post">
+                                                                    <div class="modal-body">
+                                                                        <div class="row">
+                                                                            <div class="col-md-12">
+                                                                                <div class="form-group" style="text-align: left">
+                                                                                    <label for="" class="text-align-left">Fecha de
+                                                                                        Informe</label>
+                                                                                        <input type="text" name="docente_id" value="<?=$docente_id;?>" hidden>
+                                                                                    <input type="date" name="fecha_informe" class="form-control" name=""
+                                                                                        id="">
+                                                                                </div>
                                                                             </div>
                                                                         </div>
-                                                                    </div>
-                                                                    <div class="row">
-                                                                        <div class="col-md-12">
-                                                                            <div class="form-group" style="text-align: left">
-                                                                                <label for=""
-                                                                                    class="text-align-left">Estudiante</label>
-                                                                                <select name="" class="form-control" id="">
-                                                                                    <?php
-                                                                                    foreach ($estudiantes as $estudiante) {
-                                                                                        if($estudiante['id_grado']==$asignacione['grado_id']){
-                                                                                        $id_estudiante = $estudiante['id_estudiante'];?>
-                                                                                        <option value="<?= $id_estudiante; ?>">
-                                                                                            <?= strtoupper($estudiante['apellidos'] . ", " . $estudiante['nombres']); ?>
-                                                                                        </option>
-                                                                                        <?php    
+
+                                                                        <div class="row">
+                                                                            <div class="col-md-12">
+                                                                                <div class="form-group" style="text-align: left">
+                                                                                    <label for="estudiantes-select" class="text-align-left">Estudiante</label>
+                                                                                    <select name="estudiante_id" class="form-control uppercase" id="estudiantes-select" onchange="actualizarIntegracion()">
+                                                                                        <option value="" disabled selected>Seleccione un estudiante</option>
+                                                                                        <?php
+                                                                                        foreach ($estudiantes as $estudiante) {
+                                                                                            if ($estudiante['id_grado'] == $asignacione['grado_id']) {
+                                                                                                $id_estudiante = $estudiante['id_estudiante']; ?>
+                                                                                                <option value="<?= $id_estudiante; ?>"
+                                                                                                    data-integracion="<?= $estudiante['integracion']; ?>">
+                                                                                                    <?= strtoupper($estudiante['apellidos'] . ", " . $estudiante['nombres']); ?>
+                                                                                                </option>
+                                                                                        <?php
+                                                                                            }
                                                                                         }
-                                                                                    }
-                                                                                    $id_estudiante;
-                                                                                    ?>
-                                                                                </select>
+                                                                                        ?>
+                                                                                    </select>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+
+                                                                        <script>
+                                                                            function actualizarIntegracion() {
+                                                                                // Obtener el select de estudiantes
+                                                                                const selectEstudiantes = document.getElementById('estudiantes-select');
+
+                                                                                // Obtener la opción seleccionada
+                                                                                const estudianteSeleccionado = selectEstudiantes.options[selectEstudiantes.selectedIndex];
+
+                                                                                // Obtener el valor de integración de la opción seleccionada (usamos data-integracion)
+                                                                                const integracion = estudianteSeleccionado.getAttribute('data-integracion');
+
+                                                                                // Obtener el input donde se mostrará la integración
+                                                                                const inputIntegracion = document.getElementById('integracion-input');
+
+                                                                                // Aplicar la lógica para mostrar "SI" o "NO"
+                                                                                inputIntegracion.value = integracion === 'NO' ? "NO" : "SI";
+                                                                            }
+                                                                        </script>
+
+                                                                        <!-- Aquí agregamos el input para mostrar la integración automáticamente -->
+                                                                        <div class="row" hidden>
+                                                                            <div class="col-md-12">
+                                                                                <div class="form-group" style="text-align: left">
+                                                                                    <label for="integracion-input" class="text-align-left">Integración</label>
+                                                                                    <input type="text" class="form-control" id="integracion-input" readonly required>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+
+                                                                        <div class="row" hidden>
+                                                                            <div class="col-md-12">
+                                                                                <div class="form-group" style="text-align: left">
+                                                                                    <label for="" class="text-align-left">Materia</label>
+                                                                                    <input type="text" name="materia_id" value="<?=$asignacione['id_materia'];?>">
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+
+                                                                        <div class="row">
+                                                                            <div class="col-md-12">
+                                                                                <div class="form-group" style="text-align: left">
+                                                                                    <label for="" class="text-align-left">Categorización</label>
+                                                                                    <select name="observacion" class="form-control uppercase" id="">
+                                                                                        <option value="" disabled selected>Seleccione una categorización</option>
+                                                                                        <option value="comportamiento_y_disciplina">COMPORTAMIENTO Y DISCIPLINA</option>
+                                                                                        <option value="comunicacion_y_participacion">COMUNICACIÓN Y PARTICIPACIÓN</option>
+                                                                                        <option value="diagnostico_inicial">DIAGNÓSTICO INICIAL</option>
+                                                                                        <option value="evolucion_y_progreso_general">EVOLUCIÓN Y PROGRESO GENERAL</option>
+                                                                                        <option value="observaciones_especificas">OBSERVACIONES ESPECÍFICAS</option>
+                                                                                        <option value="retos_y_estrategias_de_apoyo">RETOS Y ESTRATEGIAS DE APOYO</option>
+                                                                                        <option value="trabajo_en_grupo_y_colaboracion">TRABAJO EN GRUPO Y COLABORACIÓN</option>
+
+                                                                                    </select>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+
+                                                                        <div class="row">
+                                                                            <div class="col-md-12">
+                                                                                <div class="form-group" style="text-align: left">
+                                                                                    <label for="" class="text-align-left">Observaciones</label>
+                                                                                    <textarea name="nota" rows="10" class="form-control" id=""></textarea>
+                                                                                </div>
                                                                             </div>
                                                                         </div>
                                                                     </div>
-                                                                </div>
-                                                                <div class="modal-footer">
-                                                                    <button type="submit"
-                                                                        class="btn btn-primary">Registrar</button>
-                                                                    <button type="button" class="btn btn-secondary"
-                                                                        data-dismiss="modal">Cancelar</button>
-                                                                </div>
+                                                                    <div class="modal-footer">
+                                                                        <button type="submit" class="btn btn-info">Registrar</button>
+                                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                                                    </div>
+                                                                </form>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </td>
                                             </tr>
-                                            <?php
+                                    <?php
                                         }
                                     }
                                     ?>
