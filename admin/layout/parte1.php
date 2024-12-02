46c864119d436d246d681349b4eb998db7a6d2d6
aulaunida <proyectoaulaunida@gmail.com>
2024-12-01 23:50:51 -0300

    Correccion creación de usuarios administradores

--- admin/layout/parte1.php
+++ admin/layout/parte1.php
@@ -428,7 +428,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="<?= APP_URL; ?>/admin/usuarios" class="nav-link active">
+                                <a href="<?= APP_URL; ?>/admin/administrativos" class="nav-link active">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Consultar usuarios</p>
                                 </a>
--- admin/usuarios/edit.php
+++ admin/usuarios/edit.php
@@ -2,10 +2,10 @@
 
 $id_usuario = $_GET['id'];
 
-include ('../../app/config.php');
-include ('../../admin/layout/parte1.php');
-include ('../../app/controllers/usuarios/datos_del_usuario.php');
-include ('../../app/controllers/roles/listado_de_roles.php');
+include('../../app/config.php');
+include('../../admin/layout/parte1.php');
+include('../../app/controllers/usuarios/datos_del_usuario.php');
+include('../../app/controllers/roles/listado_de_roles.php');
 
 ?>
 
@@ -15,7 +15,7 @@ include ('../../app/controllers/roles/listado_de_roles.php');
     <div class="content">
         <div class="container">
             <div class="row">
-            <h2 style="margin-left: 20px;"><i class="bi bi-pencil-square"></i> Editar usuario: <b><?= $email ; ?></b></h2>
+                <h2 style="margin-left: 20px;"><i class="bi bi-pencil-square"></i> Editar usuario: <b><?= $email; ?></b></h2>
             </div>
             <br>
             <div class="row">
@@ -23,54 +23,50 @@ include ('../../app/controllers/roles/listado_de_roles.php');
                 <div class="col-md-12">
                     <div class="card card-outline card-success">
                         <div class="card-header">
-                        <h3 class="card-title">Editar los siguientes datos:</h3>
+                            <h3 class="card-title">Editar los siguientes datos:</h3>
                         </div>
                         <div class="card-body">
-                            <form action="<?=APP_URL;?>/app/controllers/usuarios/update.php" method="post">
+                            <form action="<?= APP_URL; ?>/app/controllers/usuarios/update.php" method="post">
                                 <div class="row">
                                     <div class="col-md-4">
                                         <div class="form-group">
                                             <label for="">Rol<b style="color:red">*</b></label>
-                                            <input type="text" name="id_usuario" value="<?=$id_usuario;?>" hidden>
-                                            <div class="form-inline">
-                                                <select name="rol_id" id="" class="form-control">
-                                                    <?php
-                                                    foreach ($roles as $role){
-                                                        $nombre_rol_tabla = $role['nombre_rol'];?>
-                                                        <option value="<?=$role['id_rol'];?>" <?php if($nombre_rol==$nombre_rol_tabla){ ?> selected="selected" <?php } ?> >
-                                                            <?=$role['nombre_rol'];?>
-                                                        </option>
-                                                        <?php
+                                            <input type="text" name="id_usuario" value="<?= $id_usuario; ?>" hidden>
+                                            <div class="form-control" readonly>
+                                                <?php
+                                                foreach ($roles as $role) {
+                                                    if ($nombre_rol == $role['nombre_rol']) {
+                                                        echo $role['nombre_rol'];
                                                     }
-                                                    ?>
-                                                </select>
-                                                <a href="<?=APP_URL;?>/admin/roles/create.php" style="margin-left: 5px" class="btn btn-primary"><i class="bi bi-file-plus"></i></a>
+                                                }
+                                                ?>
                                             </div>
+
                                         </div>
                                     </div>
                                     <!-- <div class="col-md-4">
                                         <div class="form-group">
                                         <label for="">Nombre y Apellido</label>
-                                            <input type="text" name="nombres" value="<?=$nombres;?>" class="form-control" required>
+                                            <input type="text" name="nombres" value="<?= $nombres; ?>" class="form-control" required>
                                         </div>
                                     </div> -->
                                     <div class="col-md-4">
                                         <div class="form-group">
-                                        <label for="">Correo electrónico<b style="color:red">*</b></label>
-                                            <input type="email" name="email" value="<?=$email;?>" class="form-control"  required>
+                                            <label for="">Correo electrónico<b style="color:red">*</b></label>
+                                            <input type="email" name="email" value="<?= $email; ?>" class="form-control" required>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-md-4">
                                         <div class="form-group">
-                                        <label for="">Contraseña<b style="color:red">*</b></label>
+                                            <label for="">Contraseña<b style="color:red">*</b></label>
                                             <input type="password" name="password" class="form-control">
                                         </div>
                                     </div>
                                     <div class="col-md-4">
                                         <div class="form-group">
-                                        <label for="">Repetir contraseña<b style="color:red">*</b></label>
+                                            <label for="">Repetir contraseña<b style="color:red">*</b></label>
                                             <input type="password" name="password_repet" class="form-control">
                                         </div>
                                     </div>
@@ -79,8 +75,8 @@ include ('../../app/controllers/roles/listado_de_roles.php');
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                        <button type="submit" class="btn btn-success">Actualizar</button>
-                                        <a href="<?=APP_URL;?>/admin/usuarios" class="btn btn-danger">Cancelar</a>
+                                            <button type="submit" class="btn btn-success">Actualizar</button>
+                                            <a href="<?= APP_URL; ?>/admin/usuarios" class="btn btn-danger">Cancelar</a>
                                         </div>
                                     </div>
                                 </div>
@@ -98,7 +94,7 @@ include ('../../app/controllers/roles/listado_de_roles.php');
 
 <?php
 
-include ('../../admin/layout/parte2.php');
-include ('../../layout/mensajes.php');
+include('../../admin/layout/parte2.php');
+include('../../layout/mensajes.php');
 
 ?>
