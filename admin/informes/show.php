a1f32149b6f577ea3466c885d58ceeb84a7cac50
aulaunida <proyectoaulaunida@gmail.com>
2024-12-04 10:42:54 -0300

    Arreglo de error en show.php de indicadores

--- admin/informes/show.php
+++ admin/informes/show.php
@@ -5,7 +5,7 @@ include('../../app/config.php');
 include('../../admin/layout/parte1.php');
 include('../../app/controllers/informes/datos_informes.php');
 include('../../app/controllers/estudiantes/listado_de_estudiantes.php');
-require('../../admin/librerias/fpdf186/fpdf.php');
+
 
 
 ?>
@@ -81,7 +81,6 @@ require('../../admin/librerias/fpdf186/fpdf.php');
                             <div class="row">
                             <div class="form-group">
                             <a href="<?= APP_URL; ?>/admin/informes" class="btn btn-danger">Volver</a>
-                            <a href="generar_pdf.php?id=<?= $id_informe; ?>" class="btn btn-primary" target="_blank">Exportar PDF</a>
                         </div>
                             </div>
                         </div>
