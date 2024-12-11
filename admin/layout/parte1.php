db783f98b8a5c18277a30a59cf91109dc7d192f7
aulaunida <proyectoaulaunida@gmail.com>
2024-12-10 23:55:24 -0300

    Se sacan todos los R de registrado a aula unida

--- admin/layout/parte1.php
+++ admin/layout/parte1.php
@@ -121,7 +121,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Brand Logo -->
     <a href="<?= APP_URL; ?>/admin" class="brand-link">
         <img src="<?= APP_URL; ?>/public/images/logoau.png" alt="AulaUnida Logo" class="brand-image" style="width: 36px;">
-        <span class="brand-text d-none d-sm-inline">Aula Unida®</span>
+        <span class="brand-text d-none d-sm-inline">Aula Unida</span>
     </a>
 
     <!-- Sidebar -->
